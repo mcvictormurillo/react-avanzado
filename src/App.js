b29a9184778bd39c8bcb6ceabf086fa7509d3070
VICTOR MANUEL MURILLO CAMAYO <mcvictor@unicauca.edu.co>
2020-10-25 09:32:50 -0500

    logo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { GlobalStyle } from '../GlobalStyles'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
 import { ListOftCategories } from './components/ListOftCategories'
+import { Logo } from './components/Logo'
 export const App = () => (
   <div>
     <GlobalStyle />
+    <Logo />
     <ListOftCategories />
     <ListOfPhotoCard />
   </div>
