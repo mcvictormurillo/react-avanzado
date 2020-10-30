541f7b7835663278931096178c591a46a3679db7
VICTOR MANUEL MURILLO CAMAYO <mcvictor@unicauca.edu.co>
2020-10-30 13:59:53 -0500

    clase31

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { Home } from './pages/Home'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Detail } from './pages/Detail'
 import { NavBar } from './components/Navbar'
 import { Logo } from './components/Logo'
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false })
+}
+
 export const App = () => {
   return (
     <div>
@@ -13,8 +21,21 @@ export const App = () => {
       <Router>
         <Home path='/' />
         <Home path='pet/:id' />
-        <Detail path='detail/:detailID' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
       <NavBar />
     </div>
   )
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,2 @@
+import React from 'react'
+export const NotRegisteredUser = () => <h1>NotRegisteredUser</h1>
