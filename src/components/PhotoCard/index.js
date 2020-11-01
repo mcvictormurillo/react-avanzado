3f8cc702b0e24ca9e655475ce07c942978bcc270
VICTOR MANUEL MURILLO CAMAYO <mcvictor@unicauca.edu.co>
2020-11-01 08:47:57 -0500

    clase39

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,24 +1,20 @@
 import React from 'react'
 import { ImgWrapper, Img, Article } from './styles'
-import { useLocalStorage } from '../hooks/useLocalStorage'
 import { useNearScreen } from '../hooks/useNearScreen'
 import { useMuationToogleLike } from '../hooks/useMutationToogleLike'
 import { FavButton } from '../FavButton'
 import { Link } from '@reach/router'
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, likes = 0, liked, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const { mutation, mutationLoading, mutationError } = useMuationToogleLike()
-  const key = `like-${id}`
-  const [liked, setLiked] = useLocalStorage(key, false)
   const handleFavClick = () => {
-    !liked && mutation({
+    mutation({
       variables: {
         input: { id }
       }
     })
-    setLiked(!liked)
   }
   // console.log('{ mutation, mutationLoading, mutationError }', { mutation, mutationLoading, mutationError })
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, ApolloLink, from, HttpLink } from '@apollo/client'
 import Context from './Context'
+import { onError } from '@apollo/client/link/error'
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  const token = window.sessionStorage.getItem('token')
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    })
+  }
+  return forward(operation)
+})
+const errorMiddleware = onError(({ networkError }) => {
+  if (networkError && networkError.result.code === 'invalid_token') {
+    window.sessionStorage.removeItem('token')
+    window.location = '/user'
+  }
+})
 
 const client = new ApolloClient({
-  uri: 'https://petgram-server-mcvictor.mcvictormurillo.vercel.app/graphql',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  link: from([
+    errorMiddleware,
+    authMiddleware,
+    new HttpLink({
+      uri: 'https://petgram-server-mcvictor.mcvictormurillo.vercel.app/graphql'
+    })
+  ])
 })
 
 ReactDOM.render(
